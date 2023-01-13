a4c4a1e4463d72f25e23ae45255f1848a4051599
ジョン <jeong-jin-young@ib-system.co.jp>
2023-01-13 13:25:57 +0900

    デザイン変更

--- application/views/regi_view.php
+++ application/views/regi_view.php
@@ -57,12 +57,14 @@
                 <td>メール確認</td>
                 <td><input class="form-control" name="email1" type="email" placeholder="メールをもう一度入力してください"></td>
             </tr>
-            <tr>
-                <td><input class="btn btn-success btn-submit" type="submit" value="登録"/></td>
-            </tr>
         </tbody>
         <tfoot>
-
+            <tr>
+                <td colspan="2">
+                    <input class="btn btn-success btn-submit" type="submit" value="登録"/>
+                    <a class="btn btn-default" href="https://localhost:10443/sample/index.php/Login/index">ログインページへ</a>
+                </td>
+            </tr>
         </tfoot>
     </table>
     </form>
